3a5a58947145f37e2ddca7f17b8fbed0f270481d
vod <dannyphvo@gmail.com>
2020-12-21 12:06:24 -0600

    fix indents

--- container_with_most_water/solution.go
+++ container_with_most_water/solution.go
@@ -5,18 +5,18 @@ func maxArea(height []int) int {
     max_area, current_area := 0, 0
 	
 	// Naviagte the array in O(N) by traversing with two pointers
-    for left, right := 0, len(height)-1; left < right; {
+	for left, right := 0, len(height)-1; left < right; {
 		// Area for rectangle is L * H (H is constarained by the shorter "bar")
-        current_area = (right - left) * min(height[left], height[right])
-        max_area = max(max_area, current_area)
+		current_area = (right - left) * min(height[left], height[right])
+		max_area = max(max_area, current_area)
 		
 		// The pointer to the shorter element is moved on from
-        if height[left] < height[right] {
-            left++
-        } else {
-            right--
-        }
-    }
+		if height[left] < height[right] {
+			left++
+		} else {
+			right--
+		}
+	}
     
     return max_area
 }
